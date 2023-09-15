4f14646e0fd722fca57c8804b9f1024ad770e666
ecpieritz <ecpieritz@gmail.com>
2023-09-15 15:54:27 -0300

    Added typography

--- src/App.js
+++ src/App.js
@@ -5,6 +5,12 @@ function App() {
     <>
       <Navbar />
       <h1>It's Working</h1>
+      <h2>It's Working</h2>
+      <h3>It's Working</h3>
+      <h4>It's Working</h4>
+      <h5>It's Working</h5>
+      <h6>It's Working</h6>
+      <p>It's Working</p>
     </>
   );
 }
