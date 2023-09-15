872660519c6c59284a991212dfc005b150167c45
ecpieritz <ecpieritz@gmail.com>
2023-09-15 16:14:53 -0300

    Added Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,13 @@
 export default function Navbar() {
   return (
     <nav className="fr-navbar">
-      Navbar ok
+      <a href="/" className="fr-navbar__logo">F<span>in</span>dRecipes</a>
+
+      <div className="fr-navbar__links">
+        <a href="/about">About</a>
+        <a href="/recipes">Recipes</a>
+        <a href="/settings">Settings</a>
+      </div>
     </nav>
   )
 }
