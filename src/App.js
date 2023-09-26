0881ab581e7c833cbea1516806d0e2ea338274cc
ecpieritz <ecpieritz@gmail.com>
2023-09-26 15:27:50 -0300

    Added HeroSection + .section

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
+import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
       <Navbar />
-      <h1>It's Working</h1>
-      <h2>It's Working</h2>
-      <h3>It's Working</h3>
-      <h4>It's Working</h4>
-      <h5>It's Working</h5>
-      <h6>It's Working</h6>
-      <p>It's Working</p>
+      <div className="container fr-main">
+        <HeroSection />
+      </div>
     </>
   );
 }
---
+++ src/components/HeroSection.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function HeroSection() {
+  return (
+    <div className='fr-hero section'>
+      <div className='col'>col 1</div>
+      <div className='col'>col 2</div>
+    </div>
+  )
+}
