a3545813cf46b27fdaa54377bf5f4dc74991be8b
ecpieritz <ecpieritz@gmail.com>
2023-09-26 15:46:54 -0300

    Added first Hero Col

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -3,7 +3,11 @@ import React from 'react'
 export default function HeroSection() {
   return (
     <div className='fr-hero section'>
-      <div className='col'>col 1</div>
+      <div className='col'>
+        <h1>What Are We About</h1>
+        <p>Your Gateway to Culinary Creativity! Explore a World of Flavors, One Recipe at a Time. Unleash Your Inner Chef and Elevate Home Cooking. Join Us on a Gastronomic Journey Today!</p>
+        <a className='fr-btn pink' href="/recipes">Explore Now</a>
+      </div>
       <div className='col'>col 2</div>
     </div>
   )
