389bcdf4446d6a0b35576102d255800898e167d6
ecpieritz <ecpieritz@gmail.com>
2023-09-28 14:09:14 -0300

    Added ImproveSkills js

--- src/components/ImproveSkillsSection.js
+++ src/components/ImproveSkillsSection.js
@@ -2,8 +2,23 @@ import React from 'react'
 
 function ImproveSkillsSection() {
   return (
-    <div>
-      Improve
+    <div className='fr-improve section'>
+      <div className='col fr-improve__img'>
+        <img src='/img/gallery/fr-img_10.jpg' alt="" />
+      </div>
+      <div className='col fr-improve__texts'>
+        <h1>Improve Your Skills</h1>
+
+        <ul className='fr-list__pink'>
+          <li>Learn new recipes</li>
+          <li>Experiment with food</li>
+          <li>Know nutrition facts</li>
+          <li>Get cooking tips</li>
+          <li>Get ranked</li>
+        </ul>
+
+        <a className='fr-btn pink' href="/recipes">Signup Now</a>
+      </div>
     </div>
   )
 }
