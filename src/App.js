f9c88f9f211decaed6b9ce76e379e847c08a037c
ecpieritz <ecpieritz@gmail.com>
2023-09-28 11:53:53 -0300

    Added ImproveSkills base

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import HeroSection from "./components/HeroSection";
+import ImproveSkillsSection from "./components/ImproveSkillsSection";
 import Navbar from "./components/Navbar";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
       <Navbar />
       <div className="container fr-main">
         <HeroSection />
+        <ImproveSkillsSection />
       </div>
     </>
   );
