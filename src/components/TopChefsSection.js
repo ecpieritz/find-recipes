9e60354ee817167483275d936d2f05bc6d935a09
ecpieritz <ecpieritz@gmail.com>
2023-09-29 11:39:46 -0300

    Added socials

--- src/components/TopChefsSection.js
+++ src/components/TopChefsSection.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Carousel } from 'react-responsive-carousel'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faInstagramSquare, faSquareFacebook, faTwitterSquare } from '@fortawesome/free-brands-svg-icons'
 
 function TopChefsSection() {
   const chefs = [
@@ -7,31 +9,49 @@ function TopChefsSection() {
       link: '/img/top-chiefs/fr-img_1.jpg',
       name: 'Emily Smith',
       local: 'United Kingdom',
+      instagram: 'https://www.instagram.com/ecpieritz/',
+      facebook: 'https://www.facebook.com/',
+      twitter: 'https://twitter.com/home'
     },
     {
       link: '/img/top-chiefs/fr-img_2.jpg',
       name: 'Olivia Johnson',
       local: 'France',
+      instagram: 'https://www.instagram.com/ecpieritz/',
+      facebook: 'https://www.facebook.com/',
+      twitter: 'https://twitter.com/home'
     },
     {
       link: '/img/top-chiefs/fr-img_3.jpg',
       name: 'Madison Williams',
       local: 'Germany',
+      instagram: 'https://www.instagram.com/ecpieritz/',
+      facebook: 'https://www.facebook.com/',
+      twitter: 'https://twitter.com/home'
     },
     {
       link: '/img/top-chiefs/fr-img_4.jpg',
       name: 'Ethan Davis',
       local: 'Italy',
+      instagram: 'https://www.instagram.com/ecpieritz/',
+      facebook: 'https://www.facebook.com/',
+      twitter: 'https://twitter.com/home'
     },
     {
       link: '/img/top-chiefs/fr-img_5.jpg',
       name: 'Benjamin Jones',
       local: 'Spain',
+      instagram: 'https://www.instagram.com/ecpieritz/',
+      facebook: 'https://www.facebook.com/',
+      twitter: 'https://twitter.com/home'
     },
     {
       link: '/img/top-chiefs/fr-img_6.jpg',
       name: 'Emma Brown',
       local: 'Portugal',
+      instagram: 'https://www.instagram.com/ecpieritz/',
+      facebook: 'https://www.facebook.com/',
+      twitter: 'https://twitter.com/home'
     },
   ]
   return (
@@ -44,6 +64,17 @@ function TopChefsSection() {
               <div className='fr-chefs__texts'>
               <p className='fr-chefs__texts__name'>{item.name}</p>
               <p className='fr-chefs__texts__local'>{item.local}</p>
+                <div className='fr-chefs__texts__socials'>
+                  <a href={item.instagram} target='_blank' rel="noreferrer">
+                    <FontAwesomeIcon icon={faInstagramSquare} />
+                  </a>
+                  <a href={item.facebook} target='_blank'  rel="noreferrer">
+                    <FontAwesomeIcon icon={faSquareFacebook} />
+                  </a>
+                  <a href={item.twitter} target='_blank'  rel="noreferrer">
+                    <FontAwesomeIcon icon={faTwitterSquare} />
+                  </a>
+                </div>
               </div>
             </div>
           ))
