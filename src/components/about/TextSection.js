e584af28c5234089fb260b10ff545c9d517eb121
ecpieritz <ecpieritz@gmail.com>
2023-10-09 13:05:08 -0300

    Added about text

---
+++ src/components/about/TextSection.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function TextSection() {
+  return (
+    <div className='fr-about__text section'>
+      <div className='col'>
+        <p>Born in the heart of Ananindeua, Brazil, in 2023, FindRecipes is more than just a website; it's a culinary movement driven by a passion for home cooking and a deep appreciation for flavors from around the world.</p>
+        <p>Our mission at FindRecipes is to revolutionize the way people experience food at home. We believe that everyone should have access to delicious, diverse, and easy-to-follow recipes, regardless of their culinary expertise. Our motivation stems from the desire to bring families and friends closer through the joy of cooking and sharing meals together.</p>
+        <p>With roots firmly planted in the rich culinary traditions of Brazil, we aim to celebrate the vibrant and unique flavors of our region while also inviting global tastes into our kitchens. FindRecipes is a testament to our commitment to making home cooking an exciting, accessible, and memorable experience for all. Join us on this delectable journey as we continue to inspire and empower home cooks, one recipe at a time, since 2023.</p>
+      </div>
+      <div className='col'>Imgs</div>
+    </div>
+  )
+}
+
+export default TextSection
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import HeroSection from '../components/about/HeroSection'
+import TextSection from '../components/about/TextSection'
 
 function About() {
   return (
     <>
       <HeroSection />
+      <TextSection />
     </>
   )
 }
