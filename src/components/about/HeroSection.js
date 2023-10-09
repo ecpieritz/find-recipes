f0e3e584b2b9f896ce2942f9a8a4b5a2ec3f1567
ecpieritz <ecpieritz@gmail.com>
2023-10-09 12:48:30 -0300

    Added hero bannet in the About

---
+++ src/components/about/HeroSection.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function HeroSection() {
+  return (
+    <div className='fr-about__hero'>
+      <div className='fr-hero-section__title'>
+        <h1>About Us</h1>
+      </div>
+    </div>
+  )
+}
+
+export default HeroSection
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import HeroSection from '../components/about/HeroSection'
 
 function About() {
   return (
     <>
-      About
+      <HeroSection />
     </>
   )
 }
