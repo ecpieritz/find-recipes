c5af028246e786c88b9d267ce5e57c536e54a1d6
ecpieritz <ecpieritz@gmail.com>
2023-10-09 11:17:07 -0300

    Created Home page

---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import HeroSection from "../components/HeroSection";
+import ImproveSkillsSection from "../components/ImproveSkillsSection";
+import TopChefsSection from "../components/TopChefsSection";
+
+function Home() {
+  return (
+    <div>
+      <HeroSection />
+      <ImproveSkillsSection />
+      <TopChefsSection />
+    </div>
+  )
+}
+
+export default Home
