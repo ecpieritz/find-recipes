2b4a87a38996fd4d6667e371d1bec6093be49865
unknown <ecpieritz@gmail.com>
2023-10-17 11:10:06 -0300

    Added PreviousSearches base

---
+++ src/components/recipes/PreviousSearches.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function PreviousSearches() {
+  return (
+    <div className='fr-recipes__previous-searches section'>
+      <h2>Previous Searches</h2>
+      <div className='fr-recipes__previous-searches__container'>
+
+      </div>
+    </div>
+  )
+}
+
+export default PreviousSearches
--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import PreviousSearches from '../components/recipes/PreviousSearches'
 
 function Recipes() {
   return (
     <>
-      Recipes
+      <PreviousSearches />
     </>
   )
 }
