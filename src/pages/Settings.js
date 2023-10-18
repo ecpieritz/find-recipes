597b5bc0a2dd6aa2f79889d02b4f82342e5fdb0a
unknown <ecpieritz@gmail.com>
2023-10-18 13:17:03 -0300

    Added preferred theme options

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -1,19 +1,33 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheck } from "@fortawesome/free-solid-svg-icons";
 
 function Settings() {
   return (
     <div className="fr-settings section">
       <h2>Preferred theme</h2>
       <div className="fr-settings__options-container">
-        aaaa
+        <div className="fr-settings__options-container__option">
+          <div className="check light">
+            <FontAwesomeIcon icon={faCheck} />
+          </div>
+          <div className="check dark">
+            <FontAwesomeIcon icon={faCheck} />
+          </div>
+
+        </div>
       </div>
       <h2>Primary color</h2>
       <div className="fr-settings__options-container">
-        bbbb
+        <div className="fr-settings__options-container__option">
+
+        </div>
       </div>
       <h2>Font-size</h2>
       <div className="fr-settings__options-container">
-        cccc
+        <div className="fr-settings__options-container__option">
+
+        </div>
       </div>
     </div>
   );
