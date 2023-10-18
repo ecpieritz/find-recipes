9bc8b682ca2be14e5bd018bb9f2a776d7edfa61a
unknown <ecpieritz@gmail.com>
2023-10-18 12:57:57 -0300

    Added Settings base

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from "react";
 
 function Settings() {
   return (
-    <>
-      Settings
-    </>
-  )
+    <div className="fr-settings section">
+      <h2>Preferred theme</h2>
+      <div className="fr-settings__options-container">
+        aaaa
+      </div>
+      <h2>Primary color</h2>
+      <div className="fr-settings__options-container">
+        bbbb
+      </div>
+      <h2>Font-size</h2>
+      <div className="fr-settings__options-container">
+        cccc
+      </div>
+    </div>
+  );
 }
 
-export default Settings
+export default Settings;
